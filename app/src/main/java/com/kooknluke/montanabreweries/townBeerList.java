818517f2facadf980ce69ba2a90bb44df8acfa19
Luke O'Neill <lukeo3210@gmail.com>
2016-03-04 14:46:11 -0700

    Added onclick listener to town list to test

--- app/src/main/java/com/kooknluke/montanabreweries/townBeerList.java
+++ app/src/main/java/com/kooknluke/montanabreweries/townBeerList.java
@@ -4,8 +4,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -26,6 +30,15 @@ public class townBeerList extends ActionBarActivity {
                 beerList);
 
         lv.setAdapter(arrayAdapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = ((TextView)view).getText().toString();
+
+                Toast.makeText(getBaseContext(), item, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override

95dff57b90e78dd2ab852a3a2d8be8f64f7803ff
Luke O'Neill <lukeo3210@gmail.com>
2016-05-01 15:11:42 -0600

    Cahnged town from radio buttons to list view

--- app/src/main/java/com/kooknluke/montanabreweries/Town.java
+++ app/src/main/java/com/kooknluke/montanabreweries/Town.java
@@ -3,12 +3,17 @@ package com.kooknluke.montanabreweries;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,6 +22,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -30,107 +37,87 @@ public class Town extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_town);
 
+        final Context context = this;
+        final ListView lv = (ListView) findViewById(R.id.lvTown);
         final ArrayList<String> list = new ArrayList<>();
 
-        final Context context = this;
+        ArrayList<String> townList = new ArrayList<>();
+        townList.add(0, "Belgrade");
+        townList.add(1, "Billings");
+        townList.add(2, "Bozeman");
+        townList.add(3, "Hamilton");
+        townList.add(4, "Helena");
+        townList.add(5, "Livingston");
+        townList.add(6, "Missoula");
+        townList.add(7, "Stevensville");
+        townList.add(8, "Wibaux");
 
-        final Button btnSearchTwn = (Button) findViewById(R.id.btnSearchTwn);
-
-        btnSearchTwn.setOnClickListener(new View.OnClickListener() {
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                townList){
 
             @Override
-            public void onClick(View v) {
+            public View getView(int position, View convertView,
+                                ViewGroup parent) {
+                View view =super.getView(position, convertView, parent);
 
-                String query = "SELECT+*+FROM+breweries+WHERE+name_of_town+%3D+%27" + town + "%27";
+                TextView textView=(TextView) view.findViewById(android.R.id.text1);
 
-                btnSearchTwn.setEnabled(false);
+            /*YOUR CHOICE OF COLOR*/
+                textView.setTextColor(Color.WHITE);
 
+                return view;
+            }
+        };
+
+        lv.setAdapter(arrayAdapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 try {
-                    Connection conn = new Connection();
-                    JSONArray arr = conn.connect(query);
-                    if (arr == null) {
-                        list.add("No Brewery Found");
-                    }
-                    else {
-                        for (int i = 0; i < arr.length(); i++) {
-                            JSONObject sys = arr.getJSONObject(i);
-                            String temp = sys.getString("_id");
-                            list.add(temp);
+                    String town = URLEncoder.encode(((TextView) view).getText().toString(), "UTF-8");
+
+                    String query = "SELECT+*+FROM+breweries+WHERE+name_of_town+%3D+%27" + town + "%27";
+
+                    try {
+                        Connection conn = new Connection();
+                        JSONArray arr = conn.connect(query);
+                        if (arr == null) {
+                            list.add("No Brewery Found");
+                        }
+                        else {
+                            for (int i = 0; i < arr.length(); i++) {
+                                JSONObject sys = arr.getJSONObject(i);
+                                String temp = sys.getString("_id");
+                                list.add(temp);
+                            }
                         }
                     }
-                }
-                catch (JSONException e){
-                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-                }
+                    catch (JSONException e){
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
 
-                Intent i = new Intent(context, townBeerList.class);
-                if (list.isEmpty()) {
-                    list.add("No Brewery Found");
-                    i.putStringArrayListExtra("beer", list);
-                    startActivity(i);
-                    list.clear();
-                }
-                else {
-                    i.putStringArrayListExtra("beer", list);
-                    startActivity(i);
-                    list.clear();
+                    Intent i = new Intent(context, townBeerList.class);
+                    if (list.isEmpty()) {
+                        list.add("No Brewery Found");
+                        i.putStringArrayListExtra("beer", list);
+                        startActivity(i);
+                        list.clear();
+                    }
+                    else {
+                        i.putStringArrayListExtra("beer", list);
+                        startActivity(i);
+                        list.clear();
+                    }
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
                 }
             }
         });
     }
 
-    public void onRadioButtonClicked(View view) {
-
-        boolean checked = ((RadioButton) view).isChecked();
-
-        switch(view.getId()) {
-            case R.id.rbBelgrade:
-                if (checked) {
-                    town = "Belgrade";
-                }
-                break;
-            case R.id.rbBozeman:
-                if (checked) {
-                    town = "Bozeman";
-                }
-                break;
-            case R.id.rbBillings:
-                if (checked) {
-                    town = "Billings";
-                }
-                break;
-            case R.id.rbHamilton:
-                if (checked) {
-                    town = "Hamilton";
-                }
-                break;
-            case R.id.rbHelena:
-                if (checked) {
-                    town = "Helena";
-                }
-                break;
-            case R.id.rbLivingston:
-                if (checked) {
-                    town = "Livingston";
-                }
-                break;
-            case R.id.rbMissoula:
-                if (checked) {
-                    town = "Missoula";
-                }
-                break;
-            case R.id.rbStevensville:
-                if (checked) {
-                    town = "Stevensville";
-                }
-                break;
-            case R.id.rbWibaux:
-                if (checked) {
-                    town = "Wibaux";
-                }
-                break;
-        }
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
